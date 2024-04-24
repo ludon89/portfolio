b18da5b7e5192eac063cdf7a49714f70e8b64d16
ludon89 <ludon89.dev@gmail.com>
2024-04-24 15:46:51 +0200

    ajout du JS pour le menu responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
-// console.log("hello world!");
+// =============== Navbar responsive =============== //
+// TODO fermer le menu quand on clique ailleurs
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
